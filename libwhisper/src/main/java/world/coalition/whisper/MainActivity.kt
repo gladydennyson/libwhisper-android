e330444aad0c2e2e32fd97f16e5aed4536b3d82e
Ahmad Dinkins <ahmadid2@illinois.edu>
2020-06-16 18:55:34 -0500

    Added BLE permissions

--- libwhisper/src/main/java/world/coalition/whisper/MainActivity.kt
+++ libwhisper/src/main/java/world/coalition/whisper/MainActivity.kt
@@ -1,8 +1,20 @@
 package world.coalition.whisper
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
+import android.content.Intent
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import world.coalition.whisper.agathe.android.BluetoothLE
+import world.coalition.whisper.agathe.android.BluetoothUtil
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +23,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val context: Context = applicationContext
-        val core: WhisperCore = WhisperCore()
-        core.start(context)
+
+        if (!BluetoothUtil.checkBLEPermission(context)) {
+            getBLEPermission()
+        }
+
+        Whisper.instance().start(context)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+
+    fun getBLEPermission() {
+        // request permission
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            arrayOf<String>(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            ,
+            1);
     }
 
 }
